b6211be508f4a048868989c4434867a30616b1e4
Marien Monnier <marien.monnier@goood.pro>
2016-06-19 17:05:43 +0200

    WOS-5 création de la solution de base avec un projet Web API et un projet de tests

---
+++ src/WOSAPI-WebApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WOSAPI_WebApp.Startup))]
+
+namespace WOSAPI_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
