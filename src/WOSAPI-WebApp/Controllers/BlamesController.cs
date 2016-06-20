138d17869f0065b32d288211bb5e4281aa56f774
Cedric Burceaux <cedric.burceaux@goood.pro>
2016-06-20 16:08:38 +0200

    Ajout Users / Blames dans le contexte Ajout du controleur User

--- src/WOSAPI-WebApp/Controllers/BlamesController.cs
+++ src/WOSAPI-WebApp/Controllers/BlamesController.cs
@@ -7,6 +7,7 @@ using WOSAPI.Models;
 
 namespace WOSAPI_WebApp.Controllers
 {
+    [Authorize]
     public class BlamesController : Controller
     {
         // GET /api/blames
---
+++ src/WOSAPI-WebApp/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WOSAPI.Data;
+using WOSAPI.Models;
+
+namespace WOSAPI_WebApp.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        // GET: /api/users
+        public List<User> Get()
+        {
+            using (WosContext ctx = new WosContext())
+            {
+                return ctx.Users.ToList();
+            }
+        }
+    }
+}
